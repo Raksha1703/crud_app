a5950877a06a2fe593b4f010c73bde4347386fe6
Raksha1703 <rakshabarai173@gmail.com>
2021-02-04 19:18:01 +0530

    model created

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '',
 	'username' => '',
 	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'database' => './application/db/crud_app.sqlite3',
+	'dbdriver' => 'sqlite3',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,9 +6,16 @@ class Home extends CI_Controller {
 	{
 		$this->load->view('home');
     }
-    public function display()
+    public function index()
 	{
-		$this->load->view('show');
+		$mod=$this->load->model('customermodel');
+		var_dump($mod);
+		$data=$this->customermodel->getrecords();
+		var_dump($data);
+		echo '<pre>';
+			print_r($data);
+		echo '</pre>';
+		$this->load->view('show', ['data' => $data]);
 	}
 }
 ?>
